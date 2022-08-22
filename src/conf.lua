5a21ab5c003d810c0984040fc7aa6c0b58467048
Alyxia <alyxia@riseup.net>
2022-08-22 14:52:49 +0200

    [conf] Rename to CCModManager

--- src/conf.lua
+++ src/conf.lua
@@ -166,8 +166,8 @@ function love.conf(t)
     local config = require("config")
     config.load()
 
-    t.window.title = "Olympus"
-    t.window.icon = "data/icon.png"
+    t.window.title = "CCModManager"
+    t.window.icon = "data/icon2.png"
     t.window.width = 1100
     t.window.minwidth = 1100
     t.window.height = 600
